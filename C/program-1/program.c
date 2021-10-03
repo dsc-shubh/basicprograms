6dcc3436e90a5c5be255178521314a4af17dd194
shubh shah <shubh15shah@gmail.com>
2021-10-03 15:34:43 +0530

    Update program.c

--- C/program-1/program.c
+++ C/program-1/program.c
@@ -4,8 +4,8 @@ int main()
    /*initialization */
     int n,i,j;
     printf("enter the number of rows"); /*Input the number of rows */
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)   /*condition */
+    scanf("%d",&n);/* the no of rows is n*/
+    for(i=1;i<=n;i++)   /*condition */ /*for loop*/
     {
         for(j=1;j<=i;j++)
         {
